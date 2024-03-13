d8bed39683f6bc14adb6cda08f738a0f66df4075
ryanbrian <kellyraywilliamsjr@gmail.com>
2024-03-12 21:49:32 -0500

    class is in session

--- character.cs
+++ character.cs
@@ -1,22 +1,13 @@
-using Godot;
-using System;
-
 class character{
 	class stats{
-		public int atk,spatk,def,spdef,agi,hlth,luck;
-	}
-}
-public partial class character : Node
-{
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		int atk,spatk,def,spdef,agi,hlth,luck;
+		string name;
+		enum job{
+			spanker,
+			doinker,
+			spankee,
+			zoinks
+		}
 	}
 }
 
