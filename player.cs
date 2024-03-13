a32cee2504e8d9f2690af298d6ac49b77c8e759b
ryanbrian <kellyraywilliamsjr@gmail.com>
2024-03-12 23:56:55 -0500

    it is moving

--- player.cs
+++ player.cs
@@ -15,5 +15,17 @@ public partial class player : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(Input.IsActionPressed("ui_up")){
+			Position+=new Vector2(0,-1);
+		}
+		if(Input.IsActionPressed("ui_down")){
+			Position+=new Vector2(0,1);
+		}
+		if(Input.IsActionPressed("ui_left")){
+			Position+=new Vector2(-1,0);
+		}
+		if(Input.IsActionPressed("ui_right")){
+			Position+=new Vector2(1,0);
+		}
 	}
 }
