74f7f40b3972dd6a1daf6deb193ecfd674055319
ryanbrian <kellyraywilliamsjr@gmail.com>
2024-03-12 23:25:22 -0500

    working

--- character.cs
+++ character.cs
@@ -1,4 +1,5 @@
-class character{
+using System;
+public class character{
 	class stats{
 		int atk=1,spatk=1,def=1,spdef=1,agi=1,hlth=1,luck=1;
 		string name;
--- player.cs
+++ player.cs
@@ -1,14 +1,15 @@
 using Godot;
 using System;
-using "character.cs";
+//using character;
 
 public partial class player : Sprite2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		character player;
-		print(player.atk);
+//		character player;
+//		print(player.atk);
+		GD.Print("hi");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
